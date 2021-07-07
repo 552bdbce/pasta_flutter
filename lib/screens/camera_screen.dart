310740e0f4fff8feca58ec25aa6c560717e169d1
Hiroki-Ishimaru <maru.gooooooo@gmail.com>
2021-07-07 15:56:49 +0900

    camera plugin and screen

---
+++ lib/screens/camera_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CameraScreen extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => _CameraScreen();
+  }
+
+class _CameraScreen extends State<CameraScreen>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
